82abd6d157858a13a596286ba4e5f782dda22fa7
William Lima <williamlima142014@gmail.com>
2023-05-20 18:11:00 -0300

    fetch api added

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -6,6 +6,15 @@ function App() {
   const [city, setCity] = useState("New York");
   const [weather, setWeather] = useState();
 
+  const fetchWeather = () => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6d994fb4e6fa0c9b7581cbd05a9bf307&units=metric`
+    )
+      .then((res) => res.json())
+      .then((data) => setWeather(data))
+      .catch((error) => console.log(error.message));
+  };
+
   return (
     <div className="App">
       <h1 class="text-blue-600 font-bold text-3xl">Weather App</h1>
