a08ca9b79615b7de4a2611259e4e06483dd8520c
William Lima <williamlima142014@gmail.com>
2023-05-20 16:59:31 -0300

    init tailwind

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1 class="text-blue-600 text-3xl">Weather App</h1>
     </div>
   );
 }
