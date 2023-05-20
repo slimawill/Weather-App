69f7413df60a1f9616fd26496cbf22e514f1e57e
William Lima <williamlima142014@gmail.com>
2023-05-20 18:08:20 -0300

    add usestate() and useEffect()

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [city, setCity] = useState("New York");
+  const [weather, setWeather] = useState();
+
   return (
     <div className="App">
-      <h1 class="text-blue-600 text-3xl">Weather App</h1>
+      <h1 class="text-blue-600 font-bold text-3xl">Weather App</h1>
     </div>
   );
 }
