afb900fd271e7ec9cc6f5d54c479d1aaebcd1976
William Lima <williamlima142014@gmail.com>
2023-05-20 22:04:45 -0300

    finished layout

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -22,32 +22,32 @@ function App() {
   };
 
   return (
-    <div className='app flex flex-col items-center'>
-      <h1 className="py-4 text-5xl text-white font-serif">Search Weather</h1>
-        <div className="form">
+    <div className='bg-[#1f2937] h-screen flex flex-col items-center'>
+      <h1 className="py-4 text-[#6d88ad] border-b-2 border-dotted mb-2 text-5xl font-serif">Weather App</h1>
+        <div className="py-2 px-3">
           <form onSubmit={handleSubmit}>
             <input
               type="text"
               placeholder="Enter city name"
-              className="px-4 py-3"
+              className="px-4 border-2 rounded-full py-3"
               value={city}
               onChange={(e) => setCity(e.target.value)}
               />
-            <button type="submit" className="px-4 py-3 bg-purple-500 text-white">
+            <button type="submit" className="ml-2 rounded-full px-3 py-3 bg-[#6d88ad] text-white">
               Search
             </button>
           </form>
         </div>
 
         {weather && (
-        <div className="card bg-purple-500 text-white w-[220px] h-[350px] flex flex-col justify-center items-center mt-10">
-          <h4 className="text-2xl">{weather.name}</h4>
+        <div className="rounded-lg shadow-xl bg-[#6d88ad] text-white w-[300px] h-[350px] flex flex-col justify-center items-center mt-10">
+          <h4 className="text-4xl font-bold">{weather.name}</h4>
           <img
             src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
             alt=""
             className="w-[150px]"
           />
-          <h2 className="text-5xl font-bold mb-2">{weather.main.temp}&deg;</h2>
+          <h2 className="text-4xl font-bold mb-2">{weather.main.temp}&deg;</h2>
           <p>{weather.weather[0].main}</p>
         </div>
       )}
